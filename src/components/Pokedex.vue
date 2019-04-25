4a8f16475ec86f22d4fe01e1402f14f981617566
sponee <kyle.sponheim@gmail.com>
2019-04-25 10:43:23 -0700

    pokedex and pokemon components

---
+++ src/components/Pokedex.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div v-for="pokemon in pokedex" :key="pokemon.id" class="col-md-4">
+        <Pokemon :pokemon="pokemon"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import api from "@/api";
+import Pokemon from "@/components/Pokemon";
+
+export default {
+  components: { Pokemon
+  },
+  data() {
+    return {
+      pokedex: [],
+    };
+  },
+  created() {
+    api.getPokedex().then(response => {
+      this.pokedex = response.data;
+    });
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.spinner {
+  font-size: 7rem;
+  text-align: center;
+}
+</style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card padbottom">
+    <img class="card-img-top" :src="pokemon.image_url" :alt="pokemon.name.english"></img>
+    <h5 class="card-title">{{ pokemon.name.english }}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">#{{ pokemon.id }}</h6>
+    <div class="card-body">
+      <a class="btn btn-primary" data-toggle="collapse" :href="href" role="button" aria-expanded="false" aria-controls="collapseExample">
+        Stats
+      </a>
+    </div>
+    <div class="collapse" :id="id">
+      <div class="card-body">
+        <ul v-for="(stat, statName) in pokemon.base" class="list-group list-group-flush">
+          <li class="list-group-item">{{statName}}: {{stat}}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pokemon: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    id() {
+      return this.pokemon.name.english
+    },
+    href() {
+      return "#" + this.pokemon.name.english
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+
+#padbottom {
+  padding-bottom: 30px;
+}
+</style>
