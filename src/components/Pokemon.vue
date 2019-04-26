3fd6da821f2778152a79ee1ea305629df4eb5ef4
sponee <kyle.sponheim@gmail.com>
2019-04-26 13:02:15 -0700

    pokeball styles

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -2,6 +2,7 @@
   <div class="card padbottom">
     <h5 class="card-title">{{ pokemon.english_name }}</h5>
     <h6 class="card-subtitle mb-2 text-muted">#{{ pokemon.pokedex_number }}</h6>
+    <div class="ball"><div class="button"></div></div>
   </div>
 </template>
 
@@ -29,4 +30,33 @@ export default {
 #padbottom {
   padding-bottom: 30px;
 }
+
+/* Pokeball */
+
+.ball {
+  position: relative;
+  width: 200px;
+  min-width: 50px;
+  height: 200px;
+  min-heigh: 50px;
+  margin: 30px auto;
+  border-radius: 50%;
+  border: solid;
+  background-image: -webkit-linear-gradient(-90deg, #D00 45%, #333 45%, #3f3f3f 50%, #333 55%, #FFF 55%);
+}
+
+.button {
+  position: absolute;
+  width: 20%;
+  height: 20%;
+  background-color: #FFF;
+  border-radius: 50%;
+  box-shadow: 0 0 0 3px #666, 0 0 0 10px #fff, 0 0 0 16px #3f3f3f;
+  margin: 40%;
+}
+
+.button:hover {
+  cursor: pointer;
+}
+
 </style>
